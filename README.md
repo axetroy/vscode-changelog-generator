353f1ed4fc874c86fd565376b8419c39f0b9a254
axetroy <troy450409405@gmail.com>
2019-03-10 21:40:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# vscode-changelog-generator
+
+An extension to generate changelog.
+
+## Feature
+
+- Generate changelog from beginning.
+- Generate changelog from last 1 version.
+- Generate changelog from last 2 version.
+- Generate changelog from last N version.
+- Generate `CHANGELOG.md` automatically.
+- Append this version changes to `CHANGELOG.md`.
+
+## [CHANGELOG](https://github.com/axetroy/vscode-changelog-generator/blob/master/CHANGELOG.md)
+
+## Screenshot
+
+![Screenshot](https://github.com/axetroy/vscode-changelog-generator/raw/master/screenshot.gif)
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+
+| [<img src="https://avatars1.githubusercontent.com/u/9758711?v=3" width="100px;"/><br /><sub>Axetroy</sub>](http://axetroy.github.io)<br />[💻](https://github.com/axetroy/vscode-changelog-generator/commits?author=axetroy) 🔌 [⚠️](https://github.com/axetroy/vscode-changelog-generator/commits?author=axetroy) [🐛](https://github.com/axetroy/vscode-changelog-generator/issues?q=author%3Aaxetroy) 🎨 |
+| :---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------: |
+
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## License
+
+The [MIT License](https://github.com/axetroy/vscode-changelog-generator/blob/master/LICENSE)
---
+++ src/extension.ts
@@ -0,0 +1,149 @@
+"use strict";
+import VSCODE = require("vscode");
+import * as path from "path";
+import * as execa from "execa";
+
+enum Type {
+  newDocument = "new document",
+  overwriteChangelog = "overwrite CHANGELOG.md",
+  appendToChangelog = "append to CHANGELOG.md"
+}
+
+interface Config {
+  type: string;
+  preset: string;
+  releaseCount: number;
+  changelogFileName: string;
+}
+
+export function activate(context: VSCODE.ExtensionContext) {
+  const vs: typeof VSCODE = require("vscode");
+
+  async function prickWorkspace(): Promise<VSCODE.WorkspaceFolder | undefined> {
+    const workspaces = vs.workspace.workspaceFolders;
+    if (!workspaces) {
+      return;
+    }
+    if (workspaces.length === 1) {
+      return workspaces[0];
+    }
+    if (workspaces.length > 1) {
+      return vs.window.showWorkspaceFolderPick({
+        placeHolder: "select a workspace to generate changelog"
+      });
+    }
+
+    return;
+  }
+
+  async function generate(config: Config) {
+    const workspaceFolder = await prickWorkspace();
+
+    if (!workspaceFolder) {
+      return;
+    }
+
+    const workspacePath = workspaceFolder.uri.fsPath;
+
+    const cli = path.join(
+      context.extensionPath,
+      "node_modules",
+      ".bin",
+      "conventional-changelog"
+    );
+
+    const type = config.type;
+    const isOverwriteChangelog = type === Type.overwriteChangelog;
+    const isAppend = type === Type.appendToChangelog;
+
+    const args = [
+      "--preset",
+      config.preset,
+      "--release-count",
+      config.releaseCount + "",
+      isAppend ? "--append" : "",
+      ...(isOverwriteChangelog || isAppend
+        ? ["--outfile", config.changelogFileName]
+        : [])
+    ];
+
+    const { stdout: changelog } = await execa(cli, args, {
+      cwd: workspacePath
+    });
+
+    switch (type) {
+      case Type.newDocument:
+        const document = await vs.workspace.openTextDocument({
+          language: "markdown",
+          content: changelog
+        });
+        vs.window.showTextDocument(document);
+        break;
+      case Type.overwriteChangelog:
+        break;
+      case Type.appendToChangelog:
+        break;
+      default:
+        break;
+    }
+  }
+
+  async function handler(releaseCount: number | null) {
+    const config = vs.workspace.getConfiguration("changelog");
+
+    const type = config.get<string>("type") || "";
+    const preset = config.get<string>("preset") || "";
+    const changelogFileName = config.get<string>("changelogFileName") || "";
+
+    if (releaseCount === null) {
+      releaseCount = config.get<number>("release-count") || 0;
+    }
+
+    await generate({
+      type,
+      preset,
+      releaseCount,
+      changelogFileName
+    });
+  }
+
+  vs.commands.registerCommand("changelog.generate", () => {
+    return handler(null);
+  });
+
+  vs.commands.registerCommand("changelog.generateFromLastVersion", () => {
+    return handler(1);
+  });
+
+  vs.commands.registerCommand("changelog.generateFromLastTwoVersion", () => {
+    return handler(2);
+  });
+
+  vs.commands.registerCommand(
+    "changelog.generateFromLastNVersion",
+    async () => {
+      const releaseCount = await vs.window.showInputBox({
+        placeHolder: "",
+        validateInput(input) {
+          if (/^\d+$/.test(input.trim())) {
+            return null;
+          }
+          return "Please enter an interger number.";
+        }
+      });
+      if (releaseCount === undefined) {
+        return;
+      }
+      return handler(parseInt(releaseCount, 10));
+    }
+  );
+
+  vs.commands.registerCommand("changelog.generateAll", () => {
+    return handler(0);
+  });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(context: VSCODE.ExtensionContext) {
+  //
+}
